549972e6e12890ab76e623be31ded204311a3d36
cnobles <cnobles815@gmail.com>
2016-10-07 19:12:25 -0400

    Server started.

---
+++ src/main/java/nobles/christopher/depulse/DePulseApplication.java
@@ -0,0 +1,12 @@
+package nobles.christopher.depulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DePulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DePulseApplication.class, args);
+	}
+}
