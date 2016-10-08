c0e1ec1e990b5df204921d8ddb4bb1fe6de4257d
Zac Lee <zaclee@Zac-ZCW-29.local>
2016-10-07 20:58:06 -0400

    working on http request

--- Accelerate/www/js/controllers.js
+++ Accelerate/www/js/controllers.js
@@ -4,8 +4,11 @@ angular.module('app.controllers', [])
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-
-
+	var list = ["site 1","site 2","site 3","site 4"];
+	$scope.first = list[0];
+	$scope.second = list[1];
+	$scope.third = list[2];
+	$scope.fourth = list[3];
 }])
    
 .controller('calendarCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
--- Accelerate/www/js/sandbox.js
+++ Accelerate/www/js/sandbox.js
@@ -8,3 +8,5 @@ var user = {
 	"points":"",
 	var attendedEvents = []
 };
+
+var list = [0,1,2,3,4,5,6,7,8,9];
