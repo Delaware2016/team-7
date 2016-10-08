d5f8a3b30dfcfed8803421c5cee7a8df0c156cc7
psanthil <psanthil@udel.edu>
2016-10-08 04:10:23 -0400

    edits

---
+++ src/main/java/net/codeforgood/team7/models/Score.java
@@ -0,0 +1,18 @@
+package net.codeforgood.team7.models;
+
+public class Score {
+    int points;
+
+    public Score(int s) {
+        this.points = s;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setScore(int score) {
+        this.points = score;
+    }
+
+}
