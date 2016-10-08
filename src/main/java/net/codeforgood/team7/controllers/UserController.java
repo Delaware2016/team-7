4af54d53ba89515c78339bba87ad9cc5ad16edee
Zac Lee <zaclee@Zac-ZCW-29.local>
2016-10-08 05:16:43 -0400

    check

--- src/main/java/net/codeforgood/team7/controllers/UserController.java
+++ src/main/java/net/codeforgood/team7/controllers/UserController.java
@@ -1,5 +1,7 @@
 package net.codeforgood.team7.controllers;
 
+
+import meetup.*;
 import net.codeforgood.team7.models.UserModel;
 import net.codeforgood.team7.models.UserResponse;
 import net.codeforgood.team7.respository.UserResponseRepository;
@@ -9,6 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 public class UserController {
 
@@ -28,5 +33,24 @@ public class UserController {
         Iterable<UserResponse> responses = userResponseRepository.findAll();
         return new ResponseEntity<>(responses, HttpStatus.OK);
     }
+    @RequestMapping(value = "/testClient", method = RequestMethod.GET)
+    public List testClient(){
 
-}
+        MeetupClient c;
+        boolean oauth = false;
+
+        ClientSettings settings = new ClientSettings();
+
+        settings.setMeetupKey("116528185c6768552f13a6171217fb");
+        settings.setConsumerToken(new Token("verb.for.shoe", "f5281e69ea6b3c14399b67b8"));
+        settings.setOAuthCallbackUrl("x-meetup-demo://app");
+
+        c = new MeetupClient();
+        c.setClientSettings(settings);
+        EventSearchCriteria eventSearchCriteria = new EventSearchCriteria();
+        eventSearchCriteria.setCity("Wilmington");
+        return c.getEvents(eventSearchCriteria);
+
+        }
+    }
+
